d8f369aab5ff35419bb1cc1237feb93cfb8611ae
Matthew J. Sahagian <msahagian@dotink.org>
2015-02-12 03:32:22 -0800

    Quick indentation fix

--- config/default/security.php
+++ config/default/security.php
@@ -3,13 +3,13 @@
 	return Affinity\Config::create(['providers'], [
 		'@providers' => [
 			'params' => [
-					'Inkwell\Security\JWTCookieWrapper' => [
-						':key' => 'Replace this key with a secret key'
-					],
+				'Inkwell\Security\JWTCookieWrapper' => [
+					':key' => 'Replace this key with a secret key'
+				],
 
-					'Inkwell\HTTP\Gateway\Server' => [
-						'cookie_wrapper' => 'Inkwell\Security\JWTCookieWrapper'
-					],
+				'Inkwell\HTTP\Gateway\Server' => [
+					'cookie_wrapper' => 'Inkwell\Security\JWTCookieWrapper'
+				],
 			]
 		]
 	]);
